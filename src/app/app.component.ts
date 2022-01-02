544864ad6d1f9b6d3b11fd3fa7c0993cd456ba50
Nestor Pineda <nestorpinedaelvira@gmail.com>
2022-01-02 12:23:47 +0100

    last changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { BreweryService } from './services/brewery.service';
+import { Component, OnInit } from '@angular/core';
+import { Brewery } from './models/brewery.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'NGX-Infinite-Scroll';
+  throttle = 0;
+  distance = 2;
+  page = 1;
+  breweries: Brewery[] | any[] = [];
+
+  constructor(private breweryService: BreweryService) {}
+
+  ngOnInit(): void {
+    this.breweryService
+      .getBreweries(this.page)
+      .subscribe((breweries: Brewery[]) => {
+        this.breweries = breweries;
+      });
+  }
+
+  onScroll(): void {
+    this.breweryService
+      .getBreweries(++this.page)
+      .subscribe((breweries: Brewery[]) => {
+        this.breweries.push(...breweries);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatCardModule,
+    InfiniteScrollModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
